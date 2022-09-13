58e8cea2acc3c13e4157db47f7f5f433d5e952a0
aldizams <mr.aldiza@gmail.com>
2022-09-14 00:31:19 +0700

    change controller

--- src/pinjam/routes.js
+++ src/pinjam/routes.js
@@ -4,10 +4,10 @@ const controller = require('./controller');
 const router = Router();
 
 router.get('/ruangan', controller.getRuangan);
-router.get('/jadwal', controller.getJadwal);
-router.get('/jadwal/:id', controller.getJadwalID);
-router.post('/jadwal', controller.addJadwal);
-router.delete('/jadwal/:id', controller.deleteJadwal);
-router.put('/jadwal/:id', controller.updateJadwal);
+router.get('/', controller.getJadwal);
+router.get('/:id', controller.getJadwalID);
+router.post('/', controller.addJadwal);
+router.delete('/:id', controller.deleteJadwal);
+router.put('/:id', controller.updateJadwal);
 
 module.exports = router;
