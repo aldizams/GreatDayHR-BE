d61c37801e9d43591f3666a39285ba6cba99c3d9
aldizams <mr.aldiza@gmail.com>
2022-09-14 02:30:24 +0700

    cors

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const Routes = require('./src/pinjam/routes');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.port || 5000;
+
+// cors
+app.use(
+	cors({
+		origin: true,
+		credentials: true,
+		preflightContinue: false,
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	})
+);
+app.options('*', cors());
 
 app.use(express.json());
 app.get('/', (req, res) => {
