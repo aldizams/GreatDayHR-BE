a36de1a654c8a07ff088f774b799be8f24d25046
aldizams <mr.aldiza@gmail.com>
2022-09-14 00:29:12 +0700

    setup heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Routes = require('./src/pinjam/routes');
 
 const app = express();
-const port = 3300;
+const port = process.env.port || 5000;
 
 app.use(express.json());
 app.get('/', (req, res) => {
